1e5c3e818ec1d4972a0d485667b8e67eb7c8715a
ibtissem <ibtissemibtissem0@gmail.com>
2016-10-21 12:40:53 +0100

    Update main.java

--- src/main.java
+++ src/main.java
@@ -15,13 +15,13 @@ public class main {
         cercle cerclec1 = new cercle(A, AB);
         cercle cerclec2 = new cercle(B, BC);
 
-
+        // point 
         System.out.println("A" + A.toString() + "\n B" + B.toString() + "\n C" + C.toString());
         System.out.println("\n image de A" + A.symetrie().toString() + "\n image de B" + B.symetrie().toString() + "\n image de C" + C.symetrie().toString());
-
+        //segment
         System.out.println("\n Segment AB" + AB.toString() + "\n Segment BC" + BC.toString());
         System.out.println("\n image de AB " + AB.symetrie().toString() + "\n image de BC " + BC.symetrie().toString());
-
+        //cercle
         System.out.println("\n cerclec1  " + cerclec1.toString() + "\n cerclec2  " + cerclec2.toString());
         System.out.println("\n image de c1  " + cerclec1.symetrie().toString() + "\n image de c2  " + cerclec2.symetrie().toString());
 
